859f2581df60c1c1471113667236b2ed15437a8e
unknown <solaiman.sh6@gmail.com>
2022-01-18 22:23:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import AuthProvider from './Context/AuthContext';
+import Auth from './Pages/Auth/Auth';
+import SignIn from './Pages/Auth/SignIn';
+import SignUp from './Pages/Auth/SignUp';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Cards from './Pages/Dashboard/Page/Cards';
+import NewSubmission from './Pages/Dashboard/Page/NewSubmission';
+import Profile from './Pages/Dashboard/Page/Profile';
+import Submissions from './Pages/Dashboard/Page/Submissions';
+import Wallet from './Pages/Dashboard/Page/Wallet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth/*" element={<Auth />} >
+            <Route path='sign-in' element={ <SignIn/>}/>
+            <Route path='sign-up' element={ <SignUp/>}/>
+          </Route>
+          <Route path="/dashboard/*" element={<Dashboard/>}>
+            <Route path="submission" element={<Submissions/>}/>
+            <Route path="your-cards" element={<Cards/>}/>
+            <Route path="wallet" element={<Wallet/>}/>
+            <Route path="profile" element={<Profile/>}/>
+          </Route>
+          <Route path="/dashboard/submission/new" element={<NewSubmission/>}/>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
